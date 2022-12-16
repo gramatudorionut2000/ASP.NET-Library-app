133e023aa129880aabe2dbd9ae5de6adcbb895ae
gramatudorionut2000 <grama.tudorionut@gmail.com>
2022-12-16 11:01:34 +0200

    Laborator4

---
+++ Migrations/20221213090723_Categories_show_books.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Grama_Tudor_Ionut_Lab2.Migrations
+{
+    public partial class Categories_show_books : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,10 @@
+﻿namespace Grama_Tudor_Ionut_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
